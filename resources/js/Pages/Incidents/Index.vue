7fb22ec216a74c6a2b75410dcfdad8383e199b61
Jeppenseppiro <jeffjurolan990@gmail.com>
2023-04-05 15:20:45 +0800

    setup initialization

---
+++ resources/js/Pages/Incidents/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div>{{ personnel }}</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// Props
+const props = defineProps({
+  personnel: String,
+});
+
+// Data Attributes
+const incidents = ref([
+  {
+    name: "Smelter",
+    description: "Furnace 1 overheating",
+  },
+  {
+    name: "Acid Plant",
+    description: "Acid Plant repairs",
+  },
+]);
+</script>
